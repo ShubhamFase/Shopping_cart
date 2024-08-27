1791f38f774d3c73d2f490b0d89425f2780b7534
Shubham Fase <shubhamfase2015@gmail.com>
2024-08-27 21:34:34 +0530

    add login,base,register,product and view product html pages

---
+++ src/main/java/com/ecom/conroller/HomeController.java
@@ -0,0 +1,34 @@
+package com.ecom.conroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() 
+	{
+		return"index";
+	}
+	@GetMapping("/login")
+	public String loginPage() 
+	{
+		return"login";
+	}
+	@GetMapping("/register")
+	public String registerPage() 
+	{
+		return"register";
+	}
+	@GetMapping("/product")
+	public String productrPage() 
+	{
+		return"product";
+	}
+	@GetMapping("/viewproduct")
+	public String view_product()
+	{
+		return "view_product";
+	}
+}
