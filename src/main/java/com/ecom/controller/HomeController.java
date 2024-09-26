01d75d8a7e3ac599e770844c078e77260a476fde
Shubham Fase <shubhamfase2015@gmail.com>
2024-09-26 23:30:28 +0530

    add user in to the database v-14

--- src/main/java/com/ecom/controller/HomeController.java
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -1,18 +1,33 @@
 package com.ecom.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ecom.model.Category;
 import com.ecom.model.Product;
+import com.ecom.model.UserDetails;
 import com.ecom.service.CategoryService;
 import com.ecom.service.ProductService;
+import com.ecom.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -22,6 +37,9 @@ public class HomeController {
 	
 	@Autowired
 	private ProductService productSevice;
+	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/")
 	public String home() 
@@ -55,4 +73,31 @@ public class HomeController {
 		m.addAttribute("product", productById);
 		return "view_product";
 	}
+	
+	@RequestMapping("/saveUser")
+	public String saveUser(@ModelAttribute UserDetails userDetails,@RequestParam("file") MultipartFile file,HttpSession session) throws IOException 
+	{
+		String imageName=file.isEmpty() ? "default.jpg": file.getOriginalFilename();
+		userDetails.setProfileImage(imageName);
+		UserDetails saveUser = userService.saveUser(userDetails);
+		if(!ObjectUtils.isEmpty(saveUser)) 
+		{
+			if(!file.isEmpty()) 
+			{
+				File saveFile = new ClassPathResource("static/img/").getFile();
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "profile_img" + File.separator
+						+ file.getOriginalFilename());
+				System.out.println(path);
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+				
+			}
+			session.setAttribute("success", "User saved sucessfully!!!");
+		}
+		else 
+		{
+			session.setAttribute("errormsg", "something went wrong");
+		}
+		return "redirect:/register";
+	} 
 }
---
+++ src/main/java/com/ecom/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ecom.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecom.model.UserDetails;
+import com.ecom.repository.UserRepository;
+import com.ecom.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	@Override
+	public UserDetails saveUser(UserDetails userDetails) {
+		UserDetails saveUser = userRepository.save(userDetails);
+		return saveUser;
+	}
+
+}
